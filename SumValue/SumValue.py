7eab0a4fe4c66a70182625847252bbe6304be394
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-10-28 12:57:28 +1000

    T -1 Мой первый проект
    Внес данные класса и функции суммирования чисел класса

---
+++ SumValue/SumValue.py
@@ -0,0 +1,10 @@
+﻿class MyClass:
+    a = 0
+    b = 0
+    def __init__(self, a_, b_):
+        self.a = a_
+        self.b = b_
+    def sum(self):
+        return self.a + self.b
+p = MyClass(3,5)
+print('Результат сложения чисел ', p.sum())
