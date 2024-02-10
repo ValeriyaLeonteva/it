958f4a9faaf07f86c76c83ab2b5e727b0d2b639d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-02-10 10:38:32 +1000

    My first program

---
+++ SimpeConsole/ConsoleApp1/Company.cs
@@ -0,0 +1,13 @@
+﻿public class Company
+{
+    public string title;
+
+    public Company()
+    {
+        title = "Неизвестно";
+    }
+    public Company(string t)
+    {
+        string title = t;
+    }
+}
---
+++ SimpeConsole/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿Person student = new Person();
+student.Print();
+
+student = new Person("Владимир");
+student.Print();
+
+student = new Person("Владимир", 21, "Камаз");
+student.Print();
+
+(string name, int age, string nameCompany) = student;
+
+Console.WriteLine($"Имя {name}");
